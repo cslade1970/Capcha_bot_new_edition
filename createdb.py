e724b5cd51e74f050234de2b5ab6f862ffa8464c
Denis Moskalets <denya.msk@gmail.com>
2021-01-08 12:50:46 +0100

    black and silent; kick in 2 minutes

--- createdb.py
+++ createdb.py
@@ -1,15 +1,10 @@
 import psycopg2
 
-con = psycopg2.connect(
-    database="",
-    user="",
-    password="",
-    host="",
-    port=""
-)
+con = psycopg2.connect(database="", user="", password="", host="", port="")
 
 cur = con.cursor()
-cur.execute('''
+cur.execute(
+    """
     CREATE TABLE banlist
     (id SERIAL PRIMARY KEY NOT NULL,
     user_id INT NOT NULL,
@@ -17,7 +12,8 @@ cur.execute('''
     chat_id BIGINT NOT NULL,
     captcha_message_id INT NOT NULL,
     answer INT NOT NULL);
-''')
+"""
+)
 
 con.commit()
 
